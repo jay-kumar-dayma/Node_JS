eea9626042f6fb0bc9b8643ec8295376b85676c8
jay-kumar-dayma <151022663+jay-kumar-dayma@users.noreply.github.com>
2024-11-26 04:45:13 +0000

    operation

---
+++ 01_basics/04_Operations.js
@@ -0,0 +1,17 @@
+let num = 2
+let num1 = 3
+let sum = num + num1
+console.log(sum);
+
+sum++;
+console.log(sum);
+
+
+/*
+~ in operations there must be code redability is mendatory in technical world
+~ While doing conversion we have to make sure that datatype of the element should be same
+~ ( > , < , == , >= , <= ) plays a special role in Js.
+~ === (triple equals to) is used to check the comparison strictly it includes comparison of element as well as comparison of datatypes also
+*/
+
+
