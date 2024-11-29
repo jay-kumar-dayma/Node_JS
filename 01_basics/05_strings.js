872ea97f5956a555b201e256bf1f2e928422890b
jay-kumar-dayma <151022663+jay-kumar-dayma@users.noreply.github.com>
2024-11-29 16:36:53 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,35 @@
+const name = "Jay Kumar Dayma"
+const age = 21
+console.log(`Hello my name is ${name} and my age is ${age}`);
+
+const realName = new String(" JATIN-DAYMA");
+console.log(realName);
+
+console.log(realName[4]);
+console.log(realName.__proto__);
+console.log(realName.trim());
+console.log(realName.length);
+console.log(realName.toLowerCase());
+console.log(realName.at(5));
+console.log(realName.substring(2,8));
+console.log(realName.charAt(8));
+console.log(realName.at(1));
+console.log(realName.indexOf("T"));
+console.log(realName.slice(2,8));
+console.log(realName.replace("JATIN", "JAY KUMAR "));
+console.log(realName.includes("DAYMA"));
+console.log(realName.split("-"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
